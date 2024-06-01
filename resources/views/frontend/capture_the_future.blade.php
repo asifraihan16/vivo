bb81491b3e2a799651d1495b78f2794c4c5bb02e
shahriar <srsagor007@gmail.com>
2024-06-01 22:23:34 +0600

    like

--- resources/views/frontend/capture_the_future.blade.php
+++ resources/views/frontend/capture_the_future.blade.php
@@ -50,12 +50,20 @@
                                 @foreach ($item as $moment)
                                     <div class="column is-{{ $moment->image_span_col }} branding aos-init" style="">
                                         <div class="work-item">
-                                            <div class="photo-like-area" id="photo-like-area-{{ $moment->id }}">
+                                            {{-- <div class="photo-like-area" id="photo-like-area-{{ $moment->id }}">
                                                 <a href="javascript:;" class="{{ in_array($moment->id, $user_liked_photos) ? 'liked' : 'unliked' }}"  onclick="likeCapturePhoto({{ $moment->id }})">
                                                     <i class="fa fa-heart"></i>
                                                     {{ $moment->likes_count }}
                                                 </a>
                                             
+                                        </div> --}}
+                                        <div class="photo-like-area" id="photo-like-area-{{ $moment->id }}">
+                                            
+                                            <a href="javascript:;"  class="{{ in_array($moment->id, $user_liked_photos) ? 'liked' : 'unliked' }}"  onclick="likeCapturePhoto({{ $moment->id }})" id="photo-like-btn-{{ $moment->id }}">
+                                                <i class="fa fa-heart"></i>
+                                                {{ $moment->likes_count }}
+                                            </a>
+                                        
                                         </div>
                                             <figure>
                                                 <a href="{{ url('capture_the_future_deatils/' . $moment->id) }}" {{-- class="mfp-lightbox mfp-image" --}} title="{{ $moment->title }}">
