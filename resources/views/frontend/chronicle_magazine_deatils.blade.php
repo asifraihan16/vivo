1fdb8faa45a1a10ad99e04ddb6dcf4c4d1097712
shahriar <srsagor007@gmail.com>
2024-06-03 20:30:11 +0600

    Pdf try

--- resources/views/frontend/chronicle_magazine_deatils.blade.php
+++ resources/views/frontend/chronicle_magazine_deatils.blade.php
@@ -8,10 +8,11 @@
     <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
     <!-- Include Flipbook Plugin -->
     <script src="path/to/flipbook-plugin.js"></script>
+    <link rel="stylesheet" href="{{ URL::asset('build/css/flipbook.min.css') }}">
 </head>
 <body>
     <div id="container"></div>
-
+    <script src="{{ URL::asset('build/js/flipbook.min.js') }}"></script>
     <script type="text/javascript">
         jQuery(document).ready(function () {
             var pdfUrl = @json($chronicle_magazines->file_path ? Storage::url($chronicle_magazines->file_path) : '');
